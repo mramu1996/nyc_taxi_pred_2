eb4c8023d8fa857bdb75d67e04e5376070f00b77
mramu_1996 <mandaloju.ramu@gmail.com>
2022-08-07 18:42:05 -0400

    commented out model in app.py

--- app.py
+++ app.py
@@ -3,17 +3,14 @@ import pickle
 from flask import Flask, render_template, request, jsonify
 
 # model = pickle.load(open('model.pkl', 'rb'))
-point_list = pickle.load(open('pickled_point_list.pkl', 'rb'))
+# point_list = pickle.load(open('pickled_point_list.pkl', 'rb'))
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    print(point_list)
-    # nl=point_list.split('\n')
-
-    return render_template("index.html",hell=point_list[99999])
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
@@ -25,12 +22,13 @@ if __name__ == '__main__':
 #     print("values received are - ", test_array)
 #     last = test_array[len(test_array) - 1]
 #     if last == 'Submit': test_array.pop()
-#     # TODO
+#
 
 @app.route('/android_predict', methods=['GET'])
 def android_predict():
     # req_json = request.json
     # print("Values that were posted :\n", req_json)
-    return jsonify({"Prediction": str(point_list)})
+    # jsonify({"Prediction": str(point_list)})"
+    return "atta-boy"
 
 def get(key): return request.form.get(key, 0)
